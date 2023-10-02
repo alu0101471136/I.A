c059b0b3b260064a7a5fe1878e200353a06ca1bd
Raul <alu0101471136@ull.edu.es>
2023-10-02 21:55:35 +0100

    Cambios en la impresion

--- busqueda_no_inf.cc
+++ busqueda_no_inf.cc
@@ -20,15 +20,21 @@ int main(int argc, char* argv[]) {
   ArbolBusqueda arbol(informacion_fichero);
   int opcion{0};
   std::cout << "¿Qué desea hacer?" << std::endl;
-  std::cout << "1. Busqueda en anchura" << std::endl;
+  std::cout << "1. Busqueda en amplitud" << std::endl;
   std::cout << "2. Busqueda en profundidad" << std::endl;
   std::cin >> opcion;
   std::cout << "¿Cuál es el vertice inicial?" << std::endl;
   int vertice_aux{0};
   std::cin >> vertice_aux;
+  vertice_aux--;
+  if (vertice_aux < 0 || vertice_aux >= arbol.GetNumeroVertices()) {
+    std::cout << "Vertice no válido" << std::endl;
+    return 0;
+  }
   Vertice vertice_inicial(vertice_aux);
   std::cout << "¿Cuál es el vertice final?" << std::endl;
   std::cin >> vertice_aux;
+  vertice_aux--;
   if (vertice_aux < 0 || vertice_aux >= arbol.GetNumeroVertices()) {
     std::cout << "Vertice no válido" << std::endl;
     return 0;
--- busqueda_no_inf_func.h
+++ busqueda_no_inf_func.h
@@ -14,29 +14,33 @@
 #define BUSQUEDA_NO_INF_FUNC_H
 
 #include <iostream>
+#include <fstream>
 #include "Vertice.h"
 #include "ArbolBusqueda.h"
 
 void ImprimirCamino(Vertice& vertice_inicial, Vertice& vertice_final, std::vector<Vertice>& camino, 
                     std::vector<Vertice>& vertices_visitados, std::vector<Vertice>& vertices_generados, float& distacia_camino) {
-  std::cout << "v0: " << vertice_inicial.GetId() << " | vd: " << vertice_final.GetId() << std::endl;
-  std::cout << "Camino: ";
+  std::fstream fichero_salida;
+  fichero_salida.open("salida.txt", std::ios::out);
+  fichero_salida << "Vertice inicial: " << vertice_inicial.GetId() + 1 << std::endl;
+  fichero_salida << "Vertice final: " << vertice_final.GetId() + 1 << std::endl;
+  fichero_salida << "Camino: ";
   for (int i = camino.size() - 1; i >= 0; --i) {
     if (i == 0) {
-      std::cout << camino[i].GetId();
+      fichero_salida << camino[i].GetId() + 1;
     } else {
-      std::cout << camino[i].GetId() << "-";
+      fichero_salida << camino[i].GetId() + 1 << "-";
     }
   }
-  std::cout << std::endl << "Vertices visitados: " << vertices_visitados[0].GetId();
+  fichero_salida << std::endl << "Vertices visitados: " << vertices_visitados[0].GetId() + 1;
   for (unsigned i = 1; i < vertices_visitados.size(); ++i) {
-    std::cout << ", " << vertices_visitados[i].GetId();
+    fichero_salida << ", " << vertices_visitados[i].GetId() + 1;
   }
-  std::cout << std::endl << "Vertices generados: " << vertices_generados[0].GetId();
+  fichero_salida << std::endl << "Vertices generados: " << vertices_generados[0].GetId() + 1;
   for (unsigned i = 1; i < vertices_generados.size(); ++i) {
-    std::cout << ", " << vertices_generados[i].GetId();
+    fichero_salida << ", " << vertices_generados[i].GetId() + 1;
   }
-  std::cout << "  " << std::endl << "Coste del camino: " << distacia_camino << std::endl;
+  fichero_salida << "  " << std::endl << "Coste del camino: " << distacia_camino << std::endl;
 }
 
 #endif
